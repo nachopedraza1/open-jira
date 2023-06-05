4f53c0a1100f7d522da59e3d46ac20f5e509aabe
naachopedraza1 <nachi_choco@live.com.ar>
2023-06-05 18:44:46 -0300

    save open-jira

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,6 +20,7 @@ const HomePage: NextPage = () => {
         <Grid item xs={12} sm={4} >
           <Card sx={{ height: 'calc(100vh - 100px)' }}>
             <CardHeader title="Compleatadas" />
+            <>
           </Card>
         </Grid>
       </Grid>
