5d385f8290cc63236185eba3468fd866eed78965
nachopedraza1 <nachi_choco@live.com.ar>
2023-06-09 12:56:51 -0300

    mongo.mongoose

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,24 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import { db } from "@/database";
+import { seedData } from "@/database/seed-data";
+import { Entry } from "@/models";
+import { NextApiRequest, NextApiResponse } from "next";
 
-type Data = {
-  name: string
+interface Data {
+  message: string
 }
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
+export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+
+  if (process.env.NODE_ENV === "production") {
+    return res.status(401).json({ message: "No tiene acceso a este servicio." })
+  }
+
+  db.connect();
+
+  await Entry.deleteMany();
+  await Entry.insertMany(seedData.entries)
+
+  db.disconnect();
+
+  res.status(200).json({ message: "Proceso realizado correctamente" })
 }
